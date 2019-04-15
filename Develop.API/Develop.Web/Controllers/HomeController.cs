d2b4de3423c407895a7d59e1dfae94109e554237
You suf SMAKKERZ <yusufadhi77@gmail.com>
2019-04-15 08:07:39 +0700

    Go to Github Initial Project
    Develop apps with .NET

---
+++ Develop.API/Develop.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Web.Mvc;
+using Common.Lib.RestAPI;
+using Develop.Model;
+
+namespace Develop.Web.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public virtual ActionResult Index()
+        {
+            var ListUser = RESTHelper.Get<ResultModel<List<UsersModel>>>(ConfigurationManager.AppSettings["HostAPIURL"] + ConfigurationManager.AppSettings["GetAllUser"]);
+            return View(ListUser);
+        }
+
+        public virtual ActionResult SubmitCatalogue()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public virtual ActionResult SubmitUser()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
